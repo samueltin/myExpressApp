1e817d82389e05e0244cdb1b4e0225ab0ad3a94f
samuel.tin <samuel.tin@gmail.com>
2023-03-31 10:30:19 +0100

    Add execution env parameter

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const dotenv = require('dotenv');
+dotenv.config();
+
+var executionEnv=process.env.EXECUTION_ENV
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', executionEnv:  executionEnv});
 });
 
 module.exports = router;
